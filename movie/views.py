8867004f5e018b112e11eff327ce8c3d543aceea
MauricioDCH <mdcorreah@eafit.edu.co>
2022-09-06 22:52:59 -0500

    Formulario de búsqueda

--- movie/views.py
+++ movie/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Movie
-
 # Create your views here.
 
 def home(request):
@@ -10,11 +8,7 @@ def home(request):
     # return render(request, 'home.html')
     # return render(request, 'home.html', {'name': 'Mauricio Correa'})
     searchTerm = request.GET.get('searchMovie')
-    if searchTerm:
-        movies = Movie.objects.filter(title__icontains = searchTerm)
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+    return render(request, 'home.html', {'searchTerm': searchTerm})
 
 def about(request):
     return render(request, 'about.html')
